9cbb0906d0c93f419918eb632be7d7890cefbbb6
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-06-12 22:33:46 +0800

    test(bao): native postgres-scanner cache end-to-end + FTS integration test

---
+++ plugins/bao/java/test/trexsql/datamart_native_integration_test.clj
@@ -0,0 +1,32 @@
+(ns trexsql.datamart-native-integration-test
+  "End-to-end: native DuckDB postgres-scanner copy + FTS. Requires a live
+   Postgres; set BAO_IT_PG_URL/USER/PASS/SCHEMA. Run with:
+     lein test :only trexsql.datamart-native-integration-test"
+  (:require [clojure.test :refer [deftest is testing]]
+            [trexsql.db :as db]
+            [trexsql.datamart :as datamart]))
+
+(defn- env [k] (System/getenv k))
+
+(deftest ^:integration native-postgres-copy-test
+  (when (env "BAO_IT_PG_URL")
+    (let [conn (db/create-connection {:allow-unsigned-extensions true})
+          handle (db/make-database conn {})
+          tmp (str (System/getProperty "java.io.tmpdir") "/bao-it-cache")
+          config {:database-code "it_pg"
+                  :schema-name (env "BAO_IT_PG_SCHEMA")
+                  :target-schema-name (env "BAO_IT_PG_SCHEMA")
+                  :cache-path tmp
+                  :fts-tables ["concept"]
+                  :source-credentials {:dialect "postgres"
+                                       :jdbc-url (env "BAO_IT_PG_URL")
+                                       :user (env "BAO_IT_PG_USER")
+                                       :password (env "BAO_IT_PG_PASS")}}
+          result (datamart/create-cache handle config nil)]
+      (testing "copy succeeded with at least one table"
+        (is (:success? result))
+        (is (pos? (count (:tables-copied result)))))
+      (testing "FTS index built on concept (if present in the source)"
+        (when (some #(= "concept" (:table-name %)) (:tables-copied result))
+          (is (some #{"concept"} (:fts-indexes-created result)))))
+      (db/close! handle))))
