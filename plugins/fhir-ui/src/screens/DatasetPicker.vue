56726a69f13e518e5a8e3afaff3f69ba7485451b
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-06-13 20:18:10 +0800

    feat(fhir-ui): dataset picker screen

--- plugins/fhir-ui/src/screens/DatasetPicker.vue
+++ plugins/fhir-ui/src/screens/DatasetPicker.vue
@@ -1,1 +1,21 @@
-<template><div /></template>
+<template>
+  <AtlasPageShell eyebrow="FHIR" title="Datasets">
+    <AtlasList>
+      <RouterLink v-for="d in datasets" :key="d.id" :to="`/${d.id}`" style="text-decoration:none">
+        <AtlasListItem data-dataset>{{ d.name || d.id }}</AtlasListItem>
+      </RouterLink>
+    </AtlasList>
+  </AtlasPageShell>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { AtlasPageShell, AtlasList, AtlasListItem } from "@atlas-ui";
+import { useFhir } from "@/composables/useFhir";
+const { client } = useFhir();
+const datasets = ref<any[]>([]);
+onMounted(async () => {
+  const res = await client.listDatasets();
+  datasets.value = Array.isArray(res) ? res : (res?.entry?.map((e: any) => e.resource) ?? []);
+});
+</script>
