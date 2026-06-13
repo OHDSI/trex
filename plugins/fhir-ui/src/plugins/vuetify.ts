15f9a18ff0ce5893f778740582ea12be9c36a2fa
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-06-13 19:32:28 +0800

    feat(fhir-ui): Vuetify theme from Atlas tokens

---
+++ plugins/fhir-ui/src/plugins/vuetify.ts
@@ -0,0 +1,9 @@
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import "../../vendor/atlas-ui/atlas-ui.css";
+import { createVuetify } from "vuetify";
+import { buildVuetifyOptions } from "@atlas-ui";
+
+// buildVuetifyOptions() supplies the Atlas theme (primary #1f425a, accent #eb6622,
+// compact density, outlined inputs). Falls back to a literal theme if the export shape differs.
+export const vuetify = createVuetify(buildVuetifyOptions());
